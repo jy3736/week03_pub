518e05ac26ae8a45515f85cb751626cf710fce9d
Young Yang <jyang@stust.edu.tw>
2023-09-19 06:20:38 +0000

    codespace

---
+++ week03/lab01/main.py
@@ -0,0 +1,23 @@
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+# Insert your code here
+
+min_num = a
+
+if b < min_num:
+    min_num = b
+
+if c < min_num:
+    min_num = c 
+# Insert your code here
+
+if d < min_num:
+    min_num =d 
+
+
+
+
+print(min_num)
